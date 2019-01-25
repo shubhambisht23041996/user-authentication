b892fb7969a8b5f459e7a0249ce7cd2bb14dc9a3
shubhambisht23041996 <46936434+shubhambisht23041996@users.noreply.github.com>
2019-01-25 21:21:40 +0530

    Update forms.py

--- userauthentication/authproject/testapp/forms.py
+++ userauthentication/authproject/testapp/forms.py
@@ -1,5 +1,7 @@
 from django  import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
+
+
 class signupForm(forms.ModelForm):
     class Meta:
         model=User
