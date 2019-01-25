0ad7b39e964489122e451a5b111532753122be4e
shubhambisht23041996 <46936434+shubhambisht23041996@users.noreply.github.com>
2019-01-25 21:17:57 +0530

    Update views.py

--- userauthentication/authproject/testapp/views.py
+++ userauthentication/authproject/testapp/views.py
@@ -6,13 +6,20 @@ from django.http import HttpResponseRedirect
 # Create your views here.
 def home_view(request):
     return render(request,'testapp/home.html')
+
+
 @login_required
 def python_views(request):#this is the python exam view if
-#you wat to give a test first you signup and then login the page then you give the test
+#you want to give a test first you signup and then login the page then you give the test
     return render(request,'testapp/pythonexam.html')
-@login_required
+
+
+@login_required   #decorator
 def django_view(request):
     return render(request,'testapp/djangoexam.html')
+
+
+
 def signup_view(request):#this is the signup form where you signup
     form=signupForm()
     if request.method=='POST':
@@ -22,6 +29,8 @@ def signup_view(request):#this is the signup form where you signup
         user.save()
         return HttpResponseRedirect('/accounts/login')
     return render(request,'testapp/signup.html',{'form':form})
-@login_required
+
+
+@login_required #decorator
 def apptitude_view(request):
     return render(request,'testapp/apptitude.html')
